854e29d9716b874b2d216f13b7ccf980516e2180
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-05-26 20:05:03 -0300

    feat: checando api google safe browing

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,10 +1,12 @@
-document.addEventListener("mouseover", function (event) {
+document.addEventListener("mouseover", async function (event) {
     const target = event.target;
 
     if (target.tagName === "A" && target.href) {
         const url = target.href;
 
-        if (isPhishingURL(url)) {
+        const isPhishing = await isPhishingURL(url);
+        // console.log(isPhishing);
+        if (isPhishing) {
             chrome.storage.local.get(["whitelist"]).then((data) => {
                 const whitelist = data.whitelist;
                 if (!whitelist?.includes(url)) {
@@ -15,11 +17,13 @@ document.addEventListener("mouseover", function (event) {
     }
 });
 
-function checkURL() {
+async function checkURL() {
     const url = window.location.href;
-    console.log(url)
+    // console.log(url)
 
-    if (isPhishingURL(url)) {
+    const isPhishing = await isPhishingURL(url);
+    // console.log(isPhishing);
+    if (isPhishing) {
         chrome.storage.local.get(["whitelist"]).then((data) => {
             const whitelist = data.whitelist;
             if (!whitelist?.includes(url)) {
@@ -34,6 +38,39 @@ if (document.readyState === "loading") {
     checkURL();
 }
 
-function isPhishingURL(url) {
-  return url.includes("stackoverflow.com");
+async function isPhishingURL(url) {
+//   return url.includes("stackoverflow.com"); // para teste
+    const key = undefined; // COLOQUE SUA CHAVE AQUI
+    if (key === undefined) {
+        return false;
+    }
+
+    const api_url = `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${key}`
+    const request_body = {
+        "client": {
+            "clientId": "projetodetectorphishing",
+            "clientVersion": "1.4"
+        },
+        "threatInfo": {
+            "threatTypes": ["SOCIAL_ENGINEERING", "POTENTIALLY_HARMFUL_APPLICATION"],
+            "platformTypes": ["CHROME", "WINDOWS"],
+            "threatEntryTypes": ["URL"],
+            "threatEntries": [
+                {"url": url}
+            ]
+        }
+    }
+
+    const response = await fetch(api_url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(request_body)
+    });
+
+    const data = await response.json()
+    // console.log(data);
+    // console.log(data.matches, data.matches !== undefined)
+    return data.matches !== undefined;
 }
