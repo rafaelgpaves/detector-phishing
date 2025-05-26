31469fca15c29900c1403f4d7d4d770d6d85c693
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-05-26 15:25:29 -0300

    feat: abrir popup ao entrar em site suspeito

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -15,19 +15,24 @@ document.addEventListener("mouseover", function (event) {
     }
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const url = window.location.href;
-//     console.log(url)
+function checkURL() {
+    const url = window.location.href;
+    console.log(url)
 
-//     if (isPhishingURL(url)) {
-//         chrome.storage.local.get(["whitelist"]).then((data) => {
-//             const whitelist = data.whitelist;
-//             if (!whitelist?.includes(url)) {
-//                 chrome.runtime.sendMessage({ action: "openPopup" , url: url});
-//             }
-//         })
-//     }
-// });
+    if (isPhishingURL(url)) {
+        chrome.storage.local.get(["whitelist"]).then((data) => {
+            const whitelist = data.whitelist;
+            if (!whitelist?.includes(url)) {
+                chrome.runtime.sendMessage({ action: "openPopup" , url: url});
+            }
+        })
+    }
+}
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", checkURL);
+} else {
+    checkURL();
+}
 
 function isPhishingURL(url) {
   return url.includes("stackoverflow.com");
