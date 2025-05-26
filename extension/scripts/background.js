bd7212998a541648e9e5db97515553a446ade74b
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-05-26 09:58:45 -0300

    fix: lista de urls

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -8,11 +8,14 @@ chrome.action.onClicked.addListener((tab) => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "openPopup") {
         chrome.action.openPopup();
-        var urls = chrome.storage.local.get("urlsSuspeitas");
-        if (urls.length > 0) {
-            chrome.storage.local.set({ "urlsSuspeitas" : [...urls, message.url]})
-        } else {
-            chrome.storage.local.set({ "urlsSuspeitas" : [message.url]});
-        }
+        chrome.storage.local.set({ "urlAtual" : message.url});
+        chrome.storage.local.get(["urlsSuspeitas"]).then((result) => {
+            var urls = result.urlsSuspeitas;
+            if (urls.length > 0 && !urls.includes(message.url)) {
+                chrome.storage.local.set({ "urlsSuspeitas" : [...urls, message.url]});
+            } else {
+                chrome.storage.local.set({ "urlsSuspeitas" : [message.url]});
+            }
+        });
     }
 });
--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.get("urlsSuspeitas", (data) => {
         const txt = document.getElementById("site");
-        if (data.urlsSuspeitas) {
-            txt.textContent = `${data.urlsSuspeitas} pode ser um phishing.`;
-        } else {
-            txt.textContent = "Nenhuma URL suspeita detectada.";
-        }
+        console.log(data.urlsSuspeitas);
+        chrome.storage.local.get(["urlAtual"]).then((result) => {
+            if (data.urlsSuspeitas && data.urlsSuspeitas.includes(result.urlAtual)) {
+                txt.textContent = `${result.urlAtual} pode ser um phishing.`;
+            } else {
+                txt.textContent = "Nenhuma URL suspeita detectada.";
+            }
+        });
     });
 });
